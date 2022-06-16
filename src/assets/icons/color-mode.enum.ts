b6c8dda755a6751838f0e7b1d60b4e23586ba061
Matthew <perseus362434@gmail.com>
2022-06-16 00:27:54 -0700

    fix minor

--- src/assets/icons/color-mode.enum.ts
+++ src/assets/icons/color-mode.enum.ts
@@ -1,6 +1,6 @@
 // INTEL CONFIDENTIAL
 //
-// Copyright (C) 2021 Intel Corporation
+// Copyright (C) 2022 Intel Corporation
 //
 // This software and the related documents are Intel copyrighted materials, and your use of them is governed by
 // the express license under which they were provided to you ("License"). Unless the License provides otherwise,
@@ -10,8 +10,8 @@
 // This software and the related documents are provided as is, with no express or implied warranties,
 // other than those that are expressly stated in the License.
 export enum COLOR_MODE {
-  INFORMATIVE = "var(--spectrum-global-color-informative)",
-  NEGATIVE = "var(--spectrum-global-color-negative)",
-  POSITIVE = "var(--spectrum-global-color-positive)",
-  WARNING = "var(--spectrum-global-color-warning)",
+  INFORMATIVE = 'var(--spectrum-global-color-informative)',
+  NEGATIVE = 'var(--spectrum-global-color-negative)',
+  POSITIVE = 'var(--spectrum-global-color-positive)',
+  WARNING = 'var(--spectrum-global-color-warning)',
 }
--- src/components/landing-page/landing-page-sidebar/index.ts
+++ src/components/landing-page/landing-page-sidebar/index.ts
@@ -1,6 +1,6 @@
 // INTEL CONFIDENTIAL
 //
-// Copyright (C) 2021 Intel Corporation
+// Copyright (C) 2022 Intel Corporation
 //
 // This software and the related documents are Intel copyrighted materials, and your use of them is governed by
 // the express license under which they were provided to you ("License"). Unless the License provides otherwise,
